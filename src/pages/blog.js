65c4490573ffe209bbd1a4366528998b2c4d53fb
kouta1612 <kouta1612world69@gmail.com>
2021-08-02 00:02:35 +0900

    add mdx

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -5,20 +5,26 @@ import Layout from '../components/layout'
 const BlogPage = ({ data }) => {
   return (
     <Layout pageTitle="My Blog Posts">
-      <ul>
-        { data.allFile.nodes.map(node => (
-          <li key={node.name}>{node.name}</li>
-        )) }
-      </ul>
+      { data.allMdx.nodes.map(node => (
+        <article key={node.id}>
+          <h2>{node.frontmatter.title}</h2>
+          <p>Posted: {node.frontmatter.date}</p>
+        </article>
+      )) }
     </Layout>
   )
 }
 
 export const query = graphql`
   query {
-    allFile {
+    allMdx(sort: {order: DESC, fields: frontmatter___date}) {
       nodes {
-        name
+        frontmatter {
+          title
+          date(formatString: "MMMM D, YYYY")
+        }
+        id
+        body
       }
     }
   }
